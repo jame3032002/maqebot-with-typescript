2b591fb93083219eeeee7c255dd338ce9096e62a
jame3032002 <jame3032002@hotmail.com>
2024-08-01 22:12:03 +0700

    :sparkles: feat: command can add number after L or R (For amount to turn)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import FORMULAR from "./constants/formular";
 import { RobotI } from "./types/robot";
-import { updateRobotAxis } from "./utils/general";
+import {
+  calculateDistance,
+  moveDirection,
+  updateRobotAxis,
+} from "./utils/general";
 
 const maqeBot: RobotI = {
   x: 0,
@@ -9,29 +12,100 @@ const maqeBot: RobotI = {
 };
 
 const command = process.argv.at(-1) || "";
-let distance: number[] = [];
 
-for (let i = 0; i < command.length; i++) {
-  const c = command[i].toUpperCase();
+function start({
+  maqeBot,
+  textCommand,
+}: {
+  maqeBot: RobotI;
+  textCommand: string;
+}) {
+  let newMaqeBot: RobotI = JSON.parse(JSON.stringify(maqeBot));
+  let prevCommand = "";
+  let distance: number[] = [];
 
-  if (c === "R" || c === "L") {
-    updateRobotAxis({ maqeBot, distance });
+  for (let i = 0; i < textCommand.length; i++) {
+    const cmd = textCommand[i].toUpperCase();
+    const isCurrentLeftCommand = cmd === "L";
+    const isCurrentRightCommand = cmd === "R";
+    const isCurrentWalkCommand = cmd === "W";
+    const isCurrentBackCommand = cmd === "B";
+    const isCurrentCommandNumber = /\d/.test(cmd);
+    const isFirstCommand = prevCommand === "";
+    const isPreviousLeftCommand = prevCommand === "L";
+    const isPreviousRightCommand = prevCommand === "R";
+    const isPreviousWalkCommand = prevCommand === "W";
+    const isPreviousBackCommand = prevCommand === "B";
+    const isLastCommand = i === textCommand.length - 1;
+    const isCurrentCommandNotANumber =
+      isCurrentLeftCommand ||
+      isCurrentRightCommand ||
+      isCurrentWalkCommand ||
+      isCurrentBackCommand;
 
-    const newDirection = FORMULAR[maqeBot.direction][c];
-    maqeBot.direction = newDirection;
-    distance = [];
-  } else if (c === "W") {
-    updateRobotAxis({ maqeBot, distance });
+    if (isFirstCommand && !isLastCommand) {
+      if (isCurrentCommandNotANumber) {
+        prevCommand = cmd;
+      }
+    } else {
+      if (isCurrentCommandNotANumber) {
+        if (isPreviousLeftCommand || isPreviousRightCommand) {
+          const totalDistance = calculateDistance({ distance });
+          let commandProcess = prevCommand;
 
-    distance = [];
-  } else if (/\d/.test(c) && i) {
-    distance.push(parseInt(c));
-    const isLatestCommand = i === command.length - 1;
+          if (totalDistance > 0) {
+            commandProcess = new Array(totalDistance)
+              .fill(prevCommand)
+              .join("");
+          }
 
-    if (isLatestCommand) {
-      updateRobotAxis({ maqeBot, distance });
+          newMaqeBot = moveDirection({
+            maqeBot: newMaqeBot,
+            textCommand: commandProcess,
+          });
+
+          prevCommand = cmd;
+          distance = [];
+        } else if (isPreviousWalkCommand || isPreviousBackCommand) {
+          newMaqeBot = updateRobotAxis({ maqeBot: newMaqeBot, distance });
+          prevCommand = cmd;
+          distance = [];
+        }
+
+        if (isLastCommand) {
+          newMaqeBot = moveDirection({
+            maqeBot: newMaqeBot,
+            textCommand: cmd,
+          });
+        }
+      } else if (isCurrentCommandNumber) {
+        distance.push(parseInt(cmd));
+
+        if (isLastCommand) {
+          if (isPreviousLeftCommand || isPreviousRightCommand) {
+            const totalDistance = calculateDistance({ distance });
+            let commandProcess = prevCommand;
+
+            if (totalDistance > 0) {
+              commandProcess = new Array(totalDistance)
+                .fill(prevCommand)
+                .join("");
+            }
+
+            newMaqeBot = moveDirection({
+              maqeBot: newMaqeBot,
+              textCommand: commandProcess,
+            });
+          } else if (isPreviousWalkCommand || isPreviousBackCommand) {
+            newMaqeBot = updateRobotAxis({ maqeBot: newMaqeBot, distance });
+          }
+        }
+      }
     }
   }
+
+  return newMaqeBot;
 }
 
-console.log(maqeBot);
+const newMaqeBot = start({ maqeBot, textCommand: command });
+console.log(newMaqeBot);
--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,5 +1,5 @@
 import FORMULAR from "../constants/formular";
-import { Direction, RobotI } from "../types/robot";
+import { RobotI } from "../types/robot";
 
 function calculateDistance({ distance }: { distance: number[] }): number {
   let totalDistance = 0;
@@ -18,11 +18,35 @@ function updateRobotAxis({
   distance: number[];
   maqeBot: RobotI;
 }) {
+  const newMaqeBot: RobotI = JSON.parse(JSON.stringify(maqeBot));
+
   if (distance.length > 0) {
     const totalDistance = calculateDistance({ distance });
-    const { axis, factor } = FORMULAR[maqeBot.direction];
-    maqeBot[axis] += totalDistance * factor;
+    const { axis, factor } = FORMULAR[newMaqeBot.direction];
+    newMaqeBot[axis] += totalDistance * factor;
   }
+
+  return newMaqeBot;
 }
 
-export { calculateDistance, updateRobotAxis };
+function moveDirection({
+  maqeBot,
+  textCommand,
+}: {
+  maqeBot: RobotI;
+  textCommand: string;
+}) {
+  const newMaqeBot: RobotI = JSON.parse(JSON.stringify(maqeBot));
+
+  for (let i = 0; i < textCommand.length; i++) {
+    const cmd = textCommand[i];
+
+    if (cmd === "R" || cmd === "L") {
+      newMaqeBot.direction = FORMULAR[newMaqeBot.direction][cmd];
+    }
+  }
+
+  return newMaqeBot;
+}
+
+export { calculateDistance, updateRobotAxis, moveDirection };
